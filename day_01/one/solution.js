3ec7d67545b4abccaa236d677a5af1a72195772a
andrzejchmura <chmura.and@gmail.com>
2018-12-01 15:34:55 +0100

    [01] solve

--- day_01/one/solution.js
+++ day_01/one/solution.js
@@ -1,1 +1,13 @@
 const { getInput } = require("../../utils");
+
+const input = getInput()
+  .split("")
+  .map(Number);
+
+const result = input
+  .filter(
+    (element, index, array) => element === array[(index + 1) % array.length]
+  )
+  .reduce((accumulator, value) => accumulator + value);
+
+console.log(result);
--- day_01/two/solution.js
+++ day_01/two/solution.js
@@ -1,1 +1,14 @@
 const { getInput } = require("../../utils");
+
+const input = getInput()
+  .split("")
+  .map(Number);
+
+const result = input
+  .filter(
+    (element, index, array) =>
+      element === array[(index + array.length / 2) % array.length]
+  )
+  .reduce((accumulator, value) => accumulator + value);
+
+console.log(result);
